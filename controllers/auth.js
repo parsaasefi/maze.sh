76dd3a87ac16fc8635de9b11421a5042447d4df3
Parsa Asefi <parsaasefi@protonmail.com>
2020-04-10 07:32:05 -0700

    added validation to register method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,15 @@
+const ValidationHelper = require('../helpers/validation');
+
 class AuthController {
   static registerUser(req, res) {
-    res.send('Hello User');
+    const { error: validationError } = ValidationHelper.registerValidation(
+      req.body
+    );
+
+    if (validationError)
+      return res
+        .status(400)
+        .json({ error: validationError.details[0].message.replace(/"/g, '') });
   }
 }
 
