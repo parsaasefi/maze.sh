e75f562bd9df48ef02a8c0f0a09344f43f101bd9
Parsa Asefi <parsaasefi@protonmail.com>
2020-04-10 07:41:29 -0700

    implemented register functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 const ValidationHelper = require('../helpers/validation');
+const AuthService = require('../services/auth');
 
 class AuthController {
-  static registerUser(req, res) {
+  static async registerUser(req, res) {
     const { error: validationError } = ValidationHelper.registerValidation(
       req.body
     );
@@ -10,6 +11,17 @@ class AuthController {
       return res
         .status(400)
         .json({ error: validationError.details[0].message.replace(/"/g, '') });
+
+    try {
+      const name = req.body.name.toLowerCase().trim();
+      const email = req.body.email.toLowerCase().trim();
+      const password = req.body.name.trim();
+      const user = await AuthService.registerUser(name, email, password);
+
+      return res.json(user);
+    } catch (err) {
+      return res.json({ error: err.message });
+    }
   }
 }
 
