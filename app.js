302ad6223c16236cb734099a961b1c03ea2f04b3
Parsa Asefi <parsaasefi@protonmail.com>
2020-04-10 07:21:45 -0700

    added mongodb connection

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const apiRoute = require('./routes/api');
 
+const mongoConfig = require('./config/mongo');
+
 const app = express();
+
+mongoose
+  .connect(mongoConfig.uri)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
 
 app.use(cors());
 app.use(helmet());
