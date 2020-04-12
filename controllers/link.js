9031b70936ff9c0480a4117e3382032608da2c53
Parsa Asefi <parsaasefi@protonmail.com>
2020-04-12 02:40:04 -0700

    completed link creation process

--- controllers/link.js
+++ controllers/link.js
@@ -1,13 +1,23 @@
 const LinkValidator = require('../validators/link');
+const LinkService = require('../services/link');
 
 class LinkController {
-  static createLink(req, res) {
+  static async createLink(req, res) {
     const { error: validationError } = LinkValidator.create(req.body);
 
     if (validationError)
       return res
         .status(400)
         .json({ error: validationError.details[0].message.replace(/"/g, '') });
+
+    try {
+      const destination = req.body.destination.trim();
+      const link = await LinkService.createLink(destination);
+
+      return res.json(link);
+    } catch (err) {
+      return res.status(400).json({ error: err.message });
+    }
   }
 }
 
--- services/link.js
+++ services/link.js
@@ -5,7 +5,15 @@ const URLHelper = require('../helpers/url');
 class LinkService {
   static async createLink(destination) {
     const finalDestination = await URLHelper.follow(destination);
-    const alias = LinkHelper.generateRandomAlias(6);
+    let alias = LinkHelper.generateRandomAlias(6);
+
+    while (true) {
+      const link = LinkModel.findOne({ alias });
+      if (!link) break;
+
+      alias = LinkHelper.generateRandomAlias(6);
+    }
+
     const newLink = new LinkModel({
       destination: finalDestination,
       alias,
