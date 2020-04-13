7e4aea5cb4c61b4244f80ed148cb0d559d62db12
Parsa Asefi <parsaasefi@protonmail.com>
2020-04-13 07:33:07 -0700

    changed create link route name

--- routes/api/link.js
+++ routes/api/link.js
@@ -5,6 +5,6 @@ const checkAuth = require('../../middlewares/check-auth');
 
 const router = express.Router();
 
-router.post('/', checkAuth, LinkController.createLink);
+router.post('/create', checkAuth, LinkController.createLink);
 
 module.exports = router;
