429eaa2b98630a8f374f6d4ce897d3092d9d38af
Parsa Asefi <parsaasefi@protonmail.com>
2020-04-12 21:05:12 -0700

    added uuid to links

--- models/Link.js
+++ models/Link.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
 const linkSchema = new mongoose.Schema({
   destination: {
@@ -8,6 +9,10 @@ const linkSchema = new mongoose.Schema({
   alias: {
     type: String,
     required: true,
+  },
+  uuid: {
+    type: String,
+    default: uuidv4,
   },
   clicks: {
     type: Number,
