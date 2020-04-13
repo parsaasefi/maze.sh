7975bf209fbde028ee027e2ce2a2d5a6100ee6d4
Parsa Asefi <parsaasefi@protonmail.com>
2020-04-13 07:40:57 -0700

    added comments to helpers

--- helpers/security.js
+++ helpers/security.js
@@ -5,18 +5,33 @@ const blacklist = require('../blacklist');
 const URLHelper = require('./url');
 
 class SecurityHelper {
+  /**
+   * Checks of the url exists in the blacklist
+   * @param {string} url URL to check
+   * @returns {boolean} True if it exists in the blacklist
+   */
   static async isBlacklisted(url) {
     const hosts = URLHelper.allPossibleHosts(url);
     const [res] = await blacklist.get(hosts);
-    return res.length;
+    return res.length > 0;
   }
 
+  /**
+   * Checks if the url is generated using dga
+   * @param {string} url URL to check
+   * @returns {boolean} True if it is detected as dga
+   */
   static async isDGA(url) {
     const { hostname } = URL.parse(URLHelper.removeWWW(url));
     const score = await dgadetective.checkDGA(hostname);
     return score > 100;
   }
 
+  /**
+   * Checks the url in both blacklist and dga detection
+   * @param {string} url URL to check its security
+   * @returns {object} Object contains isSafe and type
+   */
   static async check(url) {
     const isBlacklisted = await this.isBlacklisted(url);
     const isDGA = await this.isDGA(url);
--- helpers/url.js
+++ helpers/url.js
@@ -2,6 +2,11 @@ const followRedirect = require('follow-redirect-url');
 const URL = require('url');
 
 class URLHelper {
+  /**
+   * Adds protocol the url if it doesn't already have
+   * @param {string} url URL to add protocol to
+   * @returns {string} URL with protocol
+   */
   static addProtocol(url) {
     const httpPattern = /^http/i;
     const relativePattern = /^\/\//i;
@@ -11,6 +16,11 @@ class URLHelper {
     return url;
   }
 
+  /**
+   * Removes www from the hostname of the url
+   * @param {string} url URL to remove www from
+   * @returns {string} URL without www in its hostname
+   */
   static removeWWW(url) {
     const parsedURL = URL.parse(url);
     const { hostname } = parsedURL;
@@ -23,6 +33,11 @@ class URLHelper {
     return URL.format(parsedURL);
   }
 
+  /**
+   * Extracts all the possible hostname from the given url
+   * @param {string} url URL to extract hostnames from
+   * @returns {array} Contains all the possible hosts
+   */
   static allPossibleHosts(url) {
     const parsedURL = URL.parse(this.removeWWW(url));
     const { hostname } = parsedURL;
