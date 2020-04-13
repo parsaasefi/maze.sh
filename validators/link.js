9eebf5c9159058a0489bb082e63c8a3b0934ba11
Parsa Asefi <parsaasefi@protonmail.com>
2020-04-12 20:56:56 -0700

    added comments to link validator

--- validators/link.js
+++ validators/link.js
@@ -1,6 +1,11 @@
 const Joi = require('@hapi/joi');
 
 class LinkValidator {
+  /**
+   * Validates create request
+   * @param {object} data Request body
+   * @returns {object} Validation result
+   */
   static create(data) {
     const schema = Joi.object().keys({
       destination: Joi.string()
@@ -18,6 +23,11 @@ class LinkValidator {
     return schema.validate(data);
   }
 
+  /**
+   * Validates the given custom alias
+   * @param {string} alias Custom alias to be validated
+   * @returns {boolean} True if the given alias is valid
+   */
   static customAlias(alias) {
     const aliasPattern = /^[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]$/;
     return aliasPattern.test(alias);
