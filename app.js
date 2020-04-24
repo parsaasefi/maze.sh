f71c2a25635d6e480f38fce3651806b10200cb30
Parsa Asefi <parsaasefi@protonmail.com>
2020-04-24 09:09:29 -0700

    Add MongoDB connection

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const mongoConfig = require('./config/mongo');
 
 const app = express();
+
+mongoose
+  .connect(mongoConfig.uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
 
 app.use(cors());
 app.use(helmet());
