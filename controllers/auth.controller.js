524246b144d6966b026332be683bc034b217f35b
Parsa Asefi <parsaasefi@protonmail.com>
2020-04-25 08:13:52 -0700

    Add login validator

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,13 @@
+const AuthValidator = require('../validators/auth');
+
 class AuthController {
   static loginUser(req, res) {
-    res.send('Hello World');
+    const { error: validationError } = AuthValidator.login(req.body);
+
+    if (validationError)
+      return res
+        .status(400)
+        .json({ error: validationError.details[0].message.replace(/"/g, '') });
   }
 }
 
