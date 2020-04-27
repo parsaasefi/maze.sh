b14e21ab62c401e8c2d347e9ff1d7b21da2641b1
Parsa Asefi <parsaasefi@protonmail.com>
2020-04-27 06:56:57 -0700

    Add blacklist connection

---
+++ blacklist/index.js
@@ -0,0 +1,37 @@
+const mysql = require('mysql2/promise');
+const { escape } = require('sqlstring');
+
+const config = require('../config/blacklist');
+
+class Blacklist {
+  constructor() {
+    this.connection = null;
+  }
+
+  connect() {
+    const { host, port, user, password, database } = config;
+
+    return mysql
+      .createConnection({
+        host,
+        port,
+        user,
+        password,
+        database,
+      })
+      .then(connection => {
+        this.connection = connection;
+      });
+  }
+
+  get(hosts) {
+    const queryArray = ['SELECT * FROM blacklist WHERE'];
+    const searchArray = hosts.map(host => `host=${escape(host)}`);
+    const search = searchArray.join(' OR ');
+    const query = queryArray.concat(search).join(' ');
+
+    return this.connection.query(query);
+  }
+}
+
+module.exports = new Blacklist();
