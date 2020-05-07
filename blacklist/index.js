84dd6aec0c041b262e03e924c4b574cf2fdc0d63
Parsa Asefi <parsaasefi@protonmail.com>
2020-05-07 09:10:18 -0700

    Add blacklist connection

--- blacklist/index.js
+++ blacklist/index.js
@@ -24,7 +24,7 @@ class Blacklist {
       });
   }
 
-  get(hosts) {
+  findHosts(hosts) {
     const searchArray = hosts.map(host => `host=${escape(host)}`);
     const search = searchArray.join(' OR ');
 
